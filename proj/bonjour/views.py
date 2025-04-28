da8c51d3d14dc9a83ec8937cccff1516062259c2
Ubuntu <ubuntu@ip-172-31-11-246.eu-west-3.compute.internal>
2025-04-28 16:02:07 +0000

    Added proj with app bonjour

---
+++ proj/bonjour/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    context = {'title': '*B o n j o u r *',
+               'content': 'Bonjour !'}
+    return render(request,
+                  'bonjour/index.html', context)
