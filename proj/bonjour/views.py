2e7229b287432a8558cd5abe1e4c810b544ae800
Ubuntu <ubuntu@ip-172-31-11-246.eu-west-3.compute.internal>
2025-04-28 17:58:00 +0000

    Minor change to bonjour view

--- proj/bonjour/views.py
+++ proj/bonjour/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {'title': '*B o n j o u r *',
+    context = {'title': '* B o n j o u r *',
                'content': 'Bonjour !'}
     return render(request,
                   'bonjour/index.html', context)
