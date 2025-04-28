24b8c638fa775224f7d6552bb0c762ee9dda772f
Ubuntu <ubuntu@ip-172-31-11-246.eu-west-3.compute.internal>
2025-04-28 17:30:10 +0000

    Migrated to postgresql

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appdb',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',       
     }
 }
 
