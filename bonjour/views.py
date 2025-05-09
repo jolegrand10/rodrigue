a622e8587de5532b38587ad5ad036878630ad4b0
jolegrand10 <georges.georgoulis@wanadoo.fr>
2025-05-09 16:19:05 +0200

    Minor change to html content

--- bonjour/views.py
+++ bonjour/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {'title': '* B o n j o u r *',
-               'content': 'Bonjour !'}
+               'content': 'Bonjour les amis!'}
     return render(request,
                   'bonjour/index.html', context)
